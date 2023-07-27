ef1082ff1ab6e4cf8dedcece172dfb3473bf0262
Lucas Froes <78117474+lucasfroes232@users.noreply.github.com>
2023-07-27 18:29:07 -0300

    Create PilhaEncadeadaDinamica.h

---
+++ PilhaEncadeada/PilhaEncadeadaDinamica.h
@@ -0,0 +1,27 @@
+/******************************************************************************
+*******************************************************************************/
+
+//BIbliotecas
+#include <stdio.h>
+#include "PilhaEncadeadaDinamica.h"
+
+//Prototipos
+void mostraPilha(tPilha);
+int main()
+{   
+    Node n;
+    criar(&n);
+    return 0;
+}
+
+//mostra elementos
+/*void mostraPilha(tPilha p){
+    for(int i=0;i<p.topo;i++){
+        printf("%d\n",p.elementos[i]);
+    }
+}
+
+//consulta topo da pilha
+void topoPilha(tPilha p ){
+    printf("elemento no topo: %d\n",p.topo);
+}*/
