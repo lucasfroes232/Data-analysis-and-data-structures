4daaa3dfb7d7af04ad596cbf355623d5880a4ccb
Lucas Froes <78117474+lucasfroes232@users.noreply.github.com>
2023-07-27 18:22:24 -0300

    Update avl.h

--- Avl/avl.h
+++ Avl/avl.h
@@ -1,1 +1,8 @@
 
+typedef struct node *ArvAvl;
+
+ArvAvl* cria_ArvAVL();
+int removeAvl(ArvAvl *raiz,int valor);
+int insereAvl(ArvAvl *raiz,int valor);
+int consultaAvl(ArvAvl *raiz,int valor);
+void preOrdem_ArvAVL(ArvAvl *raiz);
